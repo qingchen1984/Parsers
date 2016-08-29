e2b5ad5795c46ef43283e5a4ccb94b931eb99629
vivektha <vivek.thakur@intel.com>
2016-08-29 12:11:29 +0530

    Sax subproject added along with an entity interface

---
+++ sax/src/main/java/org/parsers/sax/EntityParser.java
@@ -0,0 +1,28 @@
+package org.parsers.sax;
+
+import org.json.simple.JSONArray;
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface EntityParser {
+
+    enum WHITE_LIST {
+
+        AUTONOMY("http://schemas.autonomy.com");
+
+        private final String entity;
+        WHITE_LIST(final String entity){
+            this.entity = entity;
+        }
+        @Override
+        public String toString(){
+            return this.entity;
+        }
+    }
+
+    //List<EntityBean> parseEntities(BulletinHandler bulletinHandler) throws ParseException;
+    //JSONArray processBulletinsAsJson() throws ParseException;
+    //InputStream getInput();
+
+}
